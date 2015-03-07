0c20348bcfb66bda121f841390043331366e1853
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2015-03-07 13:55:23 +0100

    update component with last version

--- app/Plugin/CurrencyConverter/Controller/Component/CurrencyConverterComponent.php
+++ app/Plugin/CurrencyConverter/Controller/Component/CurrencyConverterComponent.php
@@ -2,8 +2,12 @@
 App::uses('Component', 'Controller', 'Session');
 
 class CurrencyConverterComponent extends Component {
-	var $controller = '';
-    var $components = array('RequestHandler');
+/**
+ * The current controller.
+ *
+ * @var string
+ */
+    public $controller = '';
 
     /**
      * Initialization to get controller variable
@@ -11,7 +15,7 @@ class CurrencyConverterComponent extends Component {
      * @param Controller $controller The controller to use.
      * @param array $settings Array of settings.
      */
-	function initialize(Controller $controller, $settings = array()) { 
+    function initialize(Controller $controller, $settings = array()) { 
         $this->controller =& $controller; 
     } 
 
@@ -27,11 +31,12 @@ class CurrencyConverterComponent extends Component {
      * @return float the total amount converted into the new currency
      */
     public function convert($fromCurrency, $toCurrency, $amount, $saveIntoDb = 1, $hourDifference = 1, $dataSource = 'default') {
-    	if($fromCurrency != $toCurrency){
+        if($fromCurrency != $toCurrency){
             $rate = 0;
 
-            if ($fromCurrency == "PDS")
+            if ($fromCurrency == "PDS"){
                 $fromCurrency = "GBP";
+            }
             
             if($saveIntoDb == 1){
                 $this->_checkIfExistTable($dataSource);
@@ -42,6 +47,7 @@ class CurrencyConverterComponent extends Component {
                 if(isset($arrReturn['find'])){
                     $find = $arrReturn['find'];
                 }
+
                 if(isset($arrReturn['rate'])){
                     $rate = $arrReturn['rate'];
                 }
@@ -50,14 +56,15 @@ class CurrencyConverterComponent extends Component {
                     $rate = $this->_getRates($fromCurrency, $toCurrency);
 
                     $CurrencyConverter->create();
-					$CurrencyConverter->set(array(
-					    'from'         => $fromCurrency,
-					    'to'           => $toCurrency,
-					    'rates'        => $rate,
-					    'created'      => date('Y-m-d H:i:s'),
+                    $CurrencyConverter->set(array(
+                        'from'         => $fromCurrency,
+                        'to'           => $toCurrency,
+                        'rates'        => $rate,
+                        'created'      => date('Y-m-d H:i:s'),
                         'modified'     => date('Y-m-d H:i:s'),
-					));
-					$CurrencyConverter->save();
+                    ));
+                    
+                    $CurrencyConverter->save();
                 }
 
                 $value = (double)$rate * (double)$amount;
@@ -156,32 +163,32 @@ class CurrencyConverterComponent extends Component {
      * @return boolean if the table standard currency_converters exist into the database
      */
     private function _checkIfExistTable($dataSource){
-    	$find = 0;
+        $find = 0;
         
-    	App::uses('ConnectionManager', 'Model');
-    	$db = ConnectionManager::getDataSource($dataSource);
-		$tables = $db->listSources();
-		foreach($tables as $t){
-			if($t == 'currency_converters'){
-				$find = 1;
+        App::uses('ConnectionManager', 'Model');
+        $db = ConnectionManager::getDataSource($dataSource);
+        $tables = $db->listSources();
+        foreach($tables as $t){
+            if($t == 'currency_converters'){
+                $find = 1;
             }
-		}
+        }
 
-		if($find == 0){
-			$sql = 'CREATE TABLE IF NOT EXISTS `currency_converters` (
-			  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-			  `from` varchar(5) NOT NULL,
-			  `to` varchar(5) NOT NULL,
-			  `rates` varchar(10) NOT NULL,
-			  `created` datetime NOT NULL,
-			  `modified` datetime NOT NULL,
-			  PRIMARY KEY (`id`)
-			) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;';
+        if($find == 0){
+            $sql = 'CREATE TABLE IF NOT EXISTS `currency_converters` (
+              `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+              `from` varchar(5) NOT NULL,
+              `to` varchar(5) NOT NULL,
+              `rates` varchar(10) NOT NULL,
+              `created` datetime NOT NULL,
+              `modified` datetime NOT NULL,
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;';
 
-			$results = $db->query($sql);
-		}
-		else{
-			return(true);
+            $results = $db->query($sql);
+        }
+        else{
+            return(true);
         }
     }
 }
